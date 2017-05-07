626cdcfd8d74d2ca8cdead2a275a669f113d8b95
Robert Schmitt <forever@thelongterm.net>
2017-05-07 07:26:20 -0700

    generic form modal to use with login and posting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,18 @@
 module ApplicationHelper
 
+  def login_form_props
+    {id: 'modal-login-form', title: 'Sign in', form: 'devise/sessions/form'}
+  end
+
+  def add_post_form_props
+    {id: 'modal-add-post-form', title: 'Add post', form: 'posts/form'}
+  end
+
+  def login_modal_form form_data
+    render 'shared/form_modal', props: login_form_props.reverse_merge(form_data: form_data) unless user_signed_in?
+  end
+
+  def add_post_modal_form form_data
+    render 'shared/form_modal', props: add_post_form_props.reverse_merge(form_data: form_data) if user_signed_in?
+  end
 end
